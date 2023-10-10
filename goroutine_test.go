8728024d5d8380b43f1be2703b48139cd35e9a57
Wahid <abdulwahidkaharr@gmail.com>
2023-10-10 13:40:18 +0700

    tes memory goroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -18,3 +18,16 @@ func TestCreateGoroutine(t *testing.T) {
 	// Note: time.Sleep untuk memberitahu aplikasi bahwa harus berhenti sejenak 1 detik untuk mengakali goroutine
 	time.Sleep(1 * time.Second)
 }
+
+// Note: ini adalah untuk membuktikan bahwa goroutine itu ukurannya sangat kecil
+func displayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go displayNumber(i) // tidak mengakibatkan memory overflow karena goroutine sangat ringan
+	}
+
+	time.Sleep(5 * time.Second)
+}
